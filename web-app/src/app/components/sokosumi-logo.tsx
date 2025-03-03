78e33d5dd3eaeb226cf5c55d359c1f5e4cbbcbd9
Andreas Osberghaus <148758+mrgrauel@users.noreply.github.com>
2025-03-03 16:50:16 +0100

    Add SVG logo image to Sokosumi logo component

--- web-app/src/app/components/sokosumi-logo.tsx
+++ web-app/src/app/components/sokosumi-logo.tsx
@@ -1,11 +1,13 @@
+import Image from "next/image";
 import Link from "next/link";
 
 export default function SokosumiLogo() {
   return (
     <div className="flex items-center">
       <Link href="/" className="text-xl font-bold">
-        Sokosumi
+        <Image src="/sokosumi-logo.svg" alt="Sokosumi Logo" width={100} height={100} />
       </Link>
     </div>
   );
 }
+
