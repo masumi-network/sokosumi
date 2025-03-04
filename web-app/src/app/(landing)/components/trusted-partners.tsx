f5c5215b507990d811ba10c319ce64331d3bdc8c
Andreas Osberghaus <148758+mrgrauel@users.noreply.github.com>
2025-03-04 14:47:05 +0100

    fixes TrustedPartners title layout

--- web-app/src/app/(landing)/components/trusted-partners.tsx
+++ web-app/src/app/(landing)/components/trusted-partners.tsx
@@ -46,7 +46,7 @@ export default function TrustedPartners({
 }: BrandLogosProps) {
     
   return (
-    <div className="relative p-6 sm:p-8">
+    <div className="container mx-auto p-6 sm:p-8">
       <div className="mt-2">
         {title && <h3 className="text-sm font-light">{title}</h3>}
       </div>
