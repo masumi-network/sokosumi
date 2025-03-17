ea448b14823e896245f563a21a8c381f372d861e
Andreas Osberghaus <148758+mrgrauel@users.noreply.github.com>
2025-03-17 09:16:18 +0100

    fix(navigation): adjust NavigationMenu alignment for better layout consistency

--- web-app/src/app/(landing)/components/navigation.tsx
+++ web-app/src/app/(landing)/components/navigation.tsx
@@ -3,7 +3,7 @@ import NavigationMenu from "./navigation-menu";
 export default function Navigation() {
   return (
     <nav className="mx-4 hidden flex-1 lg:block">
-      <NavigationMenu className="flex-row items-center justify-around gap-x-4" />
+      <NavigationMenu className="flex-row items-center justify-center gap-x-6" />
     </nav>
   );
 }
