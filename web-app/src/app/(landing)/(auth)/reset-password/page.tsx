06fee89d58f2da5844e0fd2151ebd2fe279e4122
Andreas Osberghaus <148758+mrgrauel@users.noreply.github.com>
2025-03-17 13:39:52 +0100

    refactor(auth): remove unused translation hook from reset password page

--- web-app/src/app/(landing)/(auth)/reset-password/page.tsx
+++ web-app/src/app/(landing)/(auth)/reset-password/page.tsx
@@ -1,11 +1,7 @@
-import { useTranslations } from "next-intl";
-
 import ResetPasswordForm from "./components/form";
 import ResetPasswordHeader from "./components/header";
 
 export default function ResetPasswordPage() {
-  const t = useTranslations("Auth.Pages.ResetPassword");
-
   return (
     <div className="flex flex-1 flex-col">
       <ResetPasswordHeader />
