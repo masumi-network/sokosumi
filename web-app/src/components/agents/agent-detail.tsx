f0dacaaf43970ea71879be7b02f710908e14ba28
Golddy Dev <firingdev@gmail.com>
2025-03-27 21:32:24 +0800

    change route from job to jobs

--- web-app/src/components/agents/agent-detail.tsx
+++ web-app/src/components/agents/agent-detail.tsx
@@ -115,7 +115,7 @@ function AgentDetails({ agent, className }: AgentDetailsProps) {
           </div>
           <div className="mt-auto flex flex-col gap-3">
             <div className="flex items-center gap-3">
-              <Link href={`/dashboard/job/${agent.id}?creating=true`}>
+              <Link href={`/dashboard/jobs/${agent.id}`}>
                 <Button variant="default" size="lg">
                   {t("hire")}
                 </Button>
