f911f42ed351a1af5ea7ffe5d7b858e4eb195f56
Golddy Dev <firingdev@gmail.com>
2025-03-27 18:58:04 +0800

    show latest first

--- web-app/src/app/(app)/dashboard/job/[agentId]/components/job-table/job-table.tsx
+++ web-app/src/app/(app)/dashboard/job/[agentId]/components/job-table/job-table.tsx
@@ -15,7 +15,10 @@ export default function JobTable() {
   return (
     <DataTable
       columns={columns(t, dateFormatter)}
-      data={dummyJobData}
+      data={dummyJobData.sort(
+        (a, b) =>
+          new Date(b.startedTime).getTime() - new Date(a.startedTime).getTime(),
+      )}
       containerClassName={cn(
         "w-[calc(100vw-64px)] lg:w-[max(360px,36%)] rounded-md border h-[360px] lg:h-[560px]",
       )}
