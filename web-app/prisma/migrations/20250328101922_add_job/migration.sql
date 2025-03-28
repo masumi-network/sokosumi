c1417ed61acf8684c7def99ded41abc369332ebc
Andreas Osberghaus <148758+mrgrauel@users.noreply.github.com>
2025-03-28 11:19:47 +0100

    feat(prisma): add onChainIdentifier field to Job model and create migration script

--- web-app/prisma/migrations/20250328101922_add_job/migration.sql
+++ web-app/prisma/migrations/20250328101922_add_job/migration.sql
@@ -6,6 +6,7 @@ CREATE TABLE "Job" (
     "id" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
+    "onChainIdentifier" TEXT NOT NULL,
     "agentId" TEXT NOT NULL,
     "userId" TEXT NOT NULL,
     "status" "JobStatus" NOT NULL,
@@ -19,6 +20,9 @@ CREATE TABLE "Job" (
 
     CONSTRAINT "Job_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Job_onChainIdentifier_key" ON "Job"("onChainIdentifier");
 
 -- AddForeignKey
 ALTER TABLE "Job" ADD CONSTRAINT "Job_agentId_fkey" FOREIGN KEY ("agentId") REFERENCES "Agent"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
