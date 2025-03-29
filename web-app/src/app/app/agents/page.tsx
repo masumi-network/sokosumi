166b1dc871a902832cfd13755ebe43cc3a39fbeb
Andreas Osberghaus <148758+mrgrauel@users.noreply.github.com>
2025-03-29 22:11:34 +0100

    refactor(web-app): correct import path for cached tags in agents page

--- web-app/src/app/app/agents/page.tsx
+++ web-app/src/app/app/agents/page.tsx
@@ -2,11 +2,11 @@ import { Tag } from "@prisma/client";
 import { Metadata } from "next";
 import { getTranslations } from "next-intl/server";
 
-import { getCachedTags } from "@/li, getOrCreateFavoriteAgentListb/db/services/tag.service";
 import { requireAuthentication } from "@/lib/auth/utils";
 import { AgentDTO } from "@/lib/db/dto/AgentDTO";
 import { getAgents } from "@/lib/db/services/agent.service";
 import { getOrCreateFavoriteAgentList } from "@/lib/db/services/agentList.service";
+import { getCachedTags } from "@/lib/db/services/tag.service";
 
 import FilterSection from "./components/filter-section";
 import FilteredAgents from "./components/filtered-agents";
