5951e31366552f85c5ea94d327b90cabf6db95b4
Sandro Schaier <80533393+schaier-io@users.noreply.github.com>
2025-04-01 21:50:29 +0900

    regenerate

--- web-app/prisma/migrations/20250401125022_initial/migration.sql
+++ web-app/prisma/migrations/20250401125022_initial/migration.sql
@@ -240,7 +240,7 @@ CREATE TABLE "Agent" (
     "id" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
-    "onChainIdentifier" TEXT NOT NULL,
+    "blockchainIdentifier" TEXT NOT NULL,
     "ratingId" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "overrideName" TEXT,
@@ -354,7 +354,6 @@ CREATE TABLE "Job" (
     "updatedAt" TIMESTAMP(3) NOT NULL,
     "agentJobId" TEXT NOT NULL,
     "paymentId" TEXT NOT NULL,
-    "onChainIdentifier" TEXT NOT NULL,
     "agentId" TEXT NOT NULL,
     "userId" TEXT NOT NULL,
     "status" "JobStatus" NOT NULL,
@@ -363,15 +362,13 @@ CREATE TABLE "Job" (
     "startedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "finishedAt" TIMESTAMP(3),
     "refundCreditTransactionId" TEXT,
-    "paymentTxId" TEXT,
     "creditTransactionId" TEXT NOT NULL,
     "errorNote" TEXT,
     "errorNoteKey" TEXT,
-    "blockchainIdentifier" TEXT,
+    "blockchainIdentifier" TEXT NOT NULL,
     "submitResultTime" TIMESTAMP(3),
     "unlockTime" TIMESTAMP(3),
     "externalDisputeUnlockTime" TIMESTAMP(3),
-    "agentIdentifier" TEXT,
     "sellerVkey" TEXT,
     "identifierFromPurchaser" TEXT,
 
@@ -426,7 +423,7 @@ CREATE UNIQUE INDEX "organization_slug_key" ON "organization"("slug");
 CREATE UNIQUE INDEX "AgentPricing_agentFixedPricingId_key" ON "AgentPricing"("agentFixedPricingId");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Agent_onChainIdentifier_key" ON "Agent"("onChainIdentifier");
+CREATE UNIQUE INDEX "Agent_blockchainIdentifier_key" ON "Agent"("blockchainIdentifier");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Agent_ratingId_key" ON "Agent"("ratingId");
@@ -447,13 +444,13 @@ CREATE UNIQUE INDEX "CreditTransaction_fiatPaymentId_key" ON "CreditTransaction"
 CREATE UNIQUE INDEX "FiatPayment_servicePaymentId_key" ON "FiatPayment"("servicePaymentId");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Job_onChainIdentifier_key" ON "Job"("onChainIdentifier");
-
--- CreateIndex
 CREATE UNIQUE INDEX "Job_refundCreditTransactionId_key" ON "Job"("refundCreditTransactionId");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Job_creditTransactionId_key" ON "Job"("creditTransactionId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Job_blockchainIdentifier_key" ON "Job"("blockchainIdentifier");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "CreditCost_unit_key" ON "CreditCost"("unit");
