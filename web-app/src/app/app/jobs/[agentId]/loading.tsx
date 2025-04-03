b8729c190c2f55cc1cd21eaadb87a1c77bec8b14
Sandro Schaier <80533393+schaier-io@users.noreply.github.com>
2025-04-03 17:07:08 +0900

    loading page added

--- web-app/src/app/app/jobs/[agentId]/loading.tsx
+++ web-app/src/app/app/jobs/[agentId]/loading.tsx
@@ -2,12 +2,13 @@ import { Loader2 } from "lucide-react";
 
 import { HeaderSkeleton } from "./components/header";
 
-export default function JobLoading() {
+export default function JobLoading({ right }: { right: React.ReactNode }) {
   return (
     <div className="flex flex-1 flex-col p-4 xl:p-8">
       <HeaderSkeleton />
       <div className="mt-6 flex flex-1 justify-center py-12">
         <Loader2 className="mr-2 h-8 w-8 animate-spin" />
+        {right}
       </div>
     </div>
   );
