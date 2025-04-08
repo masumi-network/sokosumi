8dfa674e15764de0553d30f34ab22d119be104f0
Andreas Osberghaus <148758+mrgrauel@users.noreply.github.com>
2025-04-08 12:43:56 +0200

    feat(env): add Stripe configuration keys to env.config.ts for payment processing

--- web-app/src/config/env.config.ts
+++ web-app/src/config/env.config.ts
@@ -14,6 +14,12 @@ const envSchemaSecrets = z.object({
     .default("false")
     .transform((val) => val === "true"),
 
+  // Stripe
+  STRIPE_PUBLISHABLE_KEY: z.string().min(1),
+  STRIPE_SECRET_KEY: z.string().min(1),
+  STRIPE_WEBHOOK_SECRET: z.string().min(1),
+  STRIPE_PRICE_ID: z.string().min(1),
+
   // Seed
   SEED_DATABASE: z
     .string()
