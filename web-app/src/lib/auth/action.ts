1fdfb28b181ca6e31ee780a11244b3c72822f0a5
Golddy Dev <firingdev@gmail.com>
2025-04-23 15:28:16 +0800

    remove auth actions and move content to lib/auth/action.ts

--- web-app/src/lib/auth/action.ts
+++ web-app/src/lib/auth/action.ts
@@ -1,5 +1,6 @@
 "use server";
-import { auth } from "@/lib/auth/auth";
+
+import { auth } from "./auth";
 
 export async function signInSocial(
   provider: "google" | "microsoft" | "apple" | "linkedin",
