6b46a42c52b83113f7aa2a7bc76630911f2ac8d8
Golddy Dev <firingdev@gmail.com>
2025-04-24 20:49:28 +0800

    add accordion to create job section

--- web-app/src/app/app/agents/[agentId]/jobs/@right/components/create-job-section.tsx
+++ web-app/src/app/app/agents/[agentId]/jobs/@right/components/create-job-section.tsx
@@ -1,11 +1,18 @@
 import Image from "next/image";
 import { useTranslations } from "next-intl";
 
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import {
   AgentWithRelations,
   CreditsPrice,
   getAgentDescription,
+  getAgentName,
 } from "@/lib/db";
 
 import { JobInputsForm } from "./job-input";
@@ -19,39 +26,88 @@ export default function CreateJobSection({
   agent,
   agentCreditsPrice,
 }: CreateJobSectionProps) {
-  const t = useTranslations("App.Agents.Jobs.CreateJob");
+  return (
+    <div className="flex h-full min-h-[300px] flex-1 flex-col">
+      <ScrollArea className="h-full">
+        <Accordion
+          type="multiple"
+          defaultValue={["information", "input"]}
+          className="w-full space-y-1.5"
+        >
+          <InformationAccordionItem agent={agent} />
+          <InputAccordionItem
+            agent={agent}
+            agentCreditsPrice={agentCreditsPrice}
+          />
+        </Accordion>
+      </ScrollArea>
+    </div>
+  );
+}
+
+function InformationAccordionItem({ agent }: { agent: AgentWithRelations }) {
+  const t = useTranslations("App.Agents.Jobs.CreateJob.Information");
+  const name = getAgentName(agent);
   const description = getAgentDescription(agent);
 
   return (
-    <div className="flex h-full min-h-[300px] flex-1 flex-col">
-      <ScrollArea className="h-[calc(100%)] rounded-md border p-4">
-        <h1 className="h-[30px] text-xl font-bold">{t("title")}</h1>
-        <div className="flex flex-1 flex-col gap-4 lg:gap-6">
-          <div className="flex flex-wrap gap-2">
-            {description && (
-              <div className="min-w-[200px] flex-1 text-base">
-                {description}
-              </div>
-            )}
-            <Image
-              src="/placeholder.svg"
-              alt="Example Output"
-              className="h-60 w-60 shrink-0 rounded-lg object-cover"
-              width={240}
-              height={240}
-            />
-          </div>
-          {/* inputs */}
-          <div className="flex flex-col gap-2">
-            <h1 className="text-xl font-bold">{t("Input.title")}</h1>
-            <p className="text-base">{t("Input.description")}</p>
-          </div>
-          <JobInputsForm
-            agentId={agent.id}
-            agentCreditsPrice={agentCreditsPrice}
-          />
+    <AccordionItemWrapper value="information" title={t("title")}>
+      <div className="flex items-center gap-6">
+        <Image
+          src="/placeholder.svg"
+          alt="Example Output"
+          className="h-36 w-36 shrink-0 rounded-lg object-cover"
+          width={90}
+          height={90}
+        />
+        <div className="flex flex-1 flex-col gap-0.5">
+          <h3 className="text-lg font-bold">{name}</h3>
+          {description && <p className="text-sm">{description}</p>}
         </div>
-      </ScrollArea>
-    </div>
+      </div>
+    </AccordionItemWrapper>
+  );
+}
+
+function InputAccordionItem({
+  agent,
+  agentCreditsPrice,
+}: {
+  agent: AgentWithRelations;
+  agentCreditsPrice: CreditsPrice;
+}) {
+  const t = useTranslations("App.Agents.Jobs.CreateJob.Input");
+
+  return (
+    <AccordionItemWrapper value="input" title={t("title")}>
+      <div className="flex flex-col gap-6">
+        <p className="text-sm">{t("description")}</p>
+        <JobInputsForm
+          agentId={agent.id}
+          agentCreditsPrice={agentCreditsPrice}
+        />
+      </div>
+    </AccordionItemWrapper>
+  );
+}
+
+function AccordionItemWrapper({
+  value,
+  title,
+  disabled = false,
+  children,
+}: {
+  value: string;
+  title: string;
+  disabled?: boolean | undefined;
+  children: React.ReactNode;
+}) {
+  return (
+    <AccordionItem value={value} className="bg-muted/50 rounded-xl border-none">
+      <AccordionTrigger className="p-4" disabled={disabled}>
+        <p className="text-base font-semibold">{title}</p>
+      </AccordionTrigger>
+      <AccordionContent className="p-4">{children}</AccordionContent>
+    </AccordionItem>
   );
 }
