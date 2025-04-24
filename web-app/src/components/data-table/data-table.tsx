ede33ef6ba9f80e8a348afe30b8a7be424d484f3
Golddy Dev <firingdev@gmail.com>
2025-04-24 20:51:58 +0800

    remove job table border and update bg color

--- web-app/src/components/data-table/data-table.tsx
+++ web-app/src/components/data-table/data-table.tsx
@@ -99,12 +99,7 @@ export default function DataTable<TData, TValue>({
       <div
         className={cn("flex flex-1 flex-col overflow-hidden", tableClassName)}
       >
-        <div
-          className={cn(
-            "bg-background sticky top-0 z-10",
-            tableHeaderClassName,
-          )}
-        >
+        <div className={cn("sticky top-0 z-10", tableHeaderClassName)}>
           <Table className="table-fixed">
             <TableHeader>
               {table.getHeaderGroups().map((headerGroup) => (
