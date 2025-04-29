7e9ce720545805ce72f92fef8bbbea233bb7cbf2
Golddy Dev <firingdev@gmail.com>
2025-04-29 15:31:45 +0800

    remove outline in agent modal

--- web-app/src/components/agents/agent-modal/agent-modal.tsx
+++ web-app/src/components/agents/agent-modal/agent-modal.tsx
@@ -67,7 +67,7 @@ function AgentModalClient({ agentList }: AgentModalProps) {
     <Dialog defaultOpen={true} onOpenChange={handleOnOpenChange}>
       <DialogPortal>
         <DialogOverlay className="backdrop-blur-lg" />
-        <DialogContent className="w-[80vw] max-w-3xl! border-none bg-transparent p-0 [&>button]:hidden">
+        <DialogContent className="w-[80vw] max-w-3xl! border-none bg-transparent p-0 focus:ring-0 focus:outline-none [&>button]:hidden">
           <DialogTitle className="hidden" />
           <DialogDescription className="hidden" />
           <ScrollArea className="max-h-[90svh]">
