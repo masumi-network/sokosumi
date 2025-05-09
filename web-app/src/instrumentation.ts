8c43b2f00422e85cb9c08eda79105b88a0133313
Andreas Osberghaus <148758+mrgrauel@users.noreply.github.com>
2025-05-09 13:37:11 +0200

    feat(sentry): integrate Sentry for error tracking and monitoring in the application

---
+++ web-app/src/instrumentation.ts
@@ -0,0 +1,15 @@
+import * as Sentry from "@sentry/nextjs";
+
+export async function register() {
+  // eslint-disable-next-line no-restricted-properties
+  if (process.env.NEXT_RUNTIME === "nodejs") {
+    await import("../sentry.server.config");
+  }
+
+  // eslint-disable-next-line no-restricted-properties
+  if (process.env.NEXT_RUNTIME === "edge") {
+    await import("../sentry.edge.config");
+  }
+}
+
+export const onRequestError = Sentry.captureRequestError;
