cdcbbb84b7ed26be929a1005fe4ffb20ff68e454
Andreas Osberghaus <148758+mrgrauel@users.noreply.github.com>
2025-05-09 12:52:14 +0200

    feat(analytics): enable tracking of file downloads and hash in Plausible provider

--- web-app/src/app/layout.tsx
+++ web-app/src/app/layout.tsx
@@ -42,6 +42,8 @@ export default async function RootLayout({
         <PlausibleProvider
           domain={getEnvSecrets().PLAUSIBLE_DOMAIN}
           trackOutboundLinks={true}
+          trackFileDownloads={true}
+          hash={true}
         />
       </head>
       <body
