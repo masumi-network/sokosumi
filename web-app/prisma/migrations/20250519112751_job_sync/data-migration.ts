87d8855fe0108ed5c68ae3c61b9be29980f31de4
Andreas Osberghaus <148758+mrgrauel@users.noreply.github.com>
2025-05-20 11:40:44 +0200

    fix(job): enhance job synchronization by filtering jobs with null agentJobStatus and adding transaction timeout settings

--- web-app/prisma/migrations/20250519112751_job_sync/data-migration.ts
+++ web-app/prisma/migrations/20250519112751_job_sync/data-migration.ts
@@ -4,22 +4,35 @@ import { PrismaClient } from "@/prisma/generated/client";
 const prisma = new PrismaClient();
 
 async function main() {
-  await prisma.$transaction(async (tx) => {
-    const jobs = await tx.job.findMany();
-    for (const job of jobs) {
-      const output = job.output;
-      if (output) {
-        const outputJson = JSON.parse(output);
-        const agentJobStatus = jobStatusToAgentJobStatus(outputJson.status);
-        await tx.job.update({
-          where: { id: job.id },
-          data: {
-            agentJobStatus: agentJobStatus,
+  await prisma.$transaction(
+    async (tx) => {
+      const jobs = await tx.job.findMany({
+        where: {
+          agentJobStatus: null,
+          output: {
+            not: null,
           },
-        });
+        },
+      });
+      for (const job of jobs) {
+        const output = job.output;
+        if (output) {
+          const outputJson = JSON.parse(output);
+          const agentJobStatus = jobStatusToAgentJobStatus(outputJson.status);
+          await tx.job.update({
+            where: { id: job.id },
+            data: {
+              agentJobStatus: agentJobStatus,
+            },
+          });
+        }
       }
-    }
-  });
+    },
+    {
+      maxWait: 10000,
+      timeout: 50000,
+    },
+  );
 }
 
 main()
