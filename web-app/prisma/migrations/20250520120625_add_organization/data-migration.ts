30047e8cec3c5b80bd7c8d3960ce5de79995f097
Golddy Dev <firingdev@gmail.com>
2025-05-20 20:35:46 +0800

    feat: add data-migration script for add-organization

---
+++ web-app/prisma/migrations/20250520120625_add_organization/data-migration.ts
@@ -0,0 +1,62 @@
+/**
+ * This migration adds an organization to the user who is not connected with any member.
+ * For each user without member, this function:
+ * - Creates an organization from user's email domain (if there is no organization with that domain)
+ * - Creates a member with that created organization and connect it to the user
+ */
+
+import { PrismaClient } from "@/prisma/generated/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  // find all users without member
+  const users = await prisma.user.findMany({
+    where: {
+      members: {
+        none: {},
+      },
+    },
+  });
+  console.log(`Found ${users.length} users without member`);
+
+  for (const user of users) {
+    await prisma.$transaction(async (tx) => {
+      const { email } = user;
+      const domain = email.split("@")[1];
+
+      // Create the organization if it doesn't exist
+      const organization = await tx.organization.upsert({
+        where: { slug: domain },
+        update: {},
+        create: {
+          slug: domain,
+          name: domain,
+        },
+      });
+
+      // Create the member
+      await tx.member.create({
+        data: {
+          user: {
+            connect: {
+              id: user.id,
+            },
+          },
+          organization: {
+            connect: {
+              id: organization.id,
+            },
+          },
+        },
+      });
+    });
+  }
+}
+
+main()
+  .catch(async (e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => await prisma.$disconnect());
