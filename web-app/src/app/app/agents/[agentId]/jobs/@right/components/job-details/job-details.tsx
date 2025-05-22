baf156d4c1dbcbab1c86921ce69b01516b8339a6
Golddy Dev <firingdev@gmail.com>
2025-05-22 17:03:27 +0800

    use radix scrollarea instead of shadcn

--- web-app/src/app/app/agents/[agentId]/jobs/@right/components/job-details/job-details.tsx
+++ web-app/src/app/app/agents/[agentId]/jobs/@right/components/job-details/job-details.tsx
@@ -1,9 +1,12 @@
+import {
+  Root as ScrollAreaRoot,
+  ScrollAreaViewport,
+} from "@radix-ui/react-scroll-area";
 import { useFormatter, useTranslations } from "next-intl";
 
 import AccordionItemWrapper from "@/app/agents/[agentId]/jobs/components/accordion-wrapper";
 import JobStatusBadge from "@/app/agents/[agentId]/jobs/components/job-status-badge";
 import { Accordion } from "@/components/ui/accordion";
-import { ScrollArea } from "@/components/ui/scroll-area";
 import { JobStatus, JobWithStatus } from "@/lib/db";
 import { cn } from "@/lib/utils";
 
@@ -24,21 +27,23 @@ export default function JobDetails({ job, className }: JobDetailsProps) {
         className,
       )}
     >
-      <ScrollArea className="h-full [&_[data-slot=scroll-area-viewport]>div]:block!">
-        <Accordion
-          type="multiple"
-          defaultValue={["input", "output"]}
-          className="w-full space-y-1.5"
-        >
-          <JobDetailsHeader createdAt={job.createdAt} status={job.status} />
-          <AccordionItemWrapper value="input" title={t("Input.title")}>
-            <JobDetailsInputs rawInput={job.input} />
-          </AccordionItemWrapper>
-          <AccordionItemWrapper value="output" title={t("Output.title")}>
-            <JobDetailsOutputs rawOutput={job.output} />
-          </AccordionItemWrapper>
-        </Accordion>
-      </ScrollArea>
+      <ScrollAreaRoot className="overflow-hidden">
+        <ScrollAreaViewport className="h-full w-full [&>div]:!block">
+          <Accordion
+            type="multiple"
+            defaultValue={["input", "output"]}
+            className="w-full space-y-1.5"
+          >
+            <JobDetailsHeader createdAt={job.createdAt} status={job.status} />
+            <AccordionItemWrapper value="input" title={t("Input.title")}>
+              <JobDetailsInputs rawInput={job.input} />
+            </AccordionItemWrapper>
+            <AccordionItemWrapper value="output" title={t("Output.title")}>
+              <JobDetailsOutputs rawOutput={job.output} />
+            </AccordionItemWrapper>
+          </Accordion>
+        </ScrollAreaViewport>
+      </ScrollAreaRoot>
     </div>
   );
 }
