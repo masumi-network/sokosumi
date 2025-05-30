2e0f2a9451abaa2b633d39d514b562e4f7215f82
Golddy Dev <firingdev@gmail.com>
2025-05-30 17:46:33 +0800

    fix: use hash function from lib

--- web-app/src/lib/api/utils.ts
+++ web-app/src/lib/api/utils.ts
@@ -1,6 +1,5 @@
-import { createHash } from "crypto";
-
 import { getEnvSecrets } from "@/config/env.config";
+import { createHash } from "@/lib/utils";
 
 export const compareApiKeys = (apiKey: string) => {
   const envApiKey = getEnvSecrets().ADMIN_KEY;
