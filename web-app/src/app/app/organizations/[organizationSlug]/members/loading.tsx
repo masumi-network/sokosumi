271cbd22652cff12dfea68954d397c1248d5f119
Golddy Dev <firingdev@gmail.com>
2025-06-05 15:58:31 +0800

    feat(loading): add loading component for organization members page

---
+++ web-app/src/app/app/organizations/[organizationSlug]/members/loading.tsx
@@ -0,0 +1,7 @@
+import DefaultLoading from "@/components/default-loading";
+
+export default function OrganizationMembersPageLoading() {
+  return (
+    <DefaultLoading className="bg-muted/50 h-full min-h-[300px] w-full flex-1 rounded-xl border-none p-8" />
+  );
+}
