96cb12829d5849465653ddf63858871be459b70d
Andreas Osberghaus <148758+mrgrauel@users.noreply.github.com>
2025-06-10 09:53:18 +0200

    chore(seed): remove user seeding logic and update environment example

--- web-app/prisma/seed.ts
+++ web-app/prisma/seed.ts
@@ -1,57 +1,13 @@
-import { v4 as uuidv4 } from "uuid";
-
 import { getEnvPublicConfig, getEnvSecrets } from "@/config/env.config";
 import { usdmUnit } from "@/lib/utils";
 import { PrismaClient } from "@/prisma/generated/client";
-
-import { hashPassword } from "./util/password";
 
 const prisma = new PrismaClient();
 
 const seedDatabase = getEnvSecrets().SEED_DATABASE;
 
-const seedUser = async (): Promise<string> => {
-  let user = await prisma.user.findFirst({
-    where: {
-      email: "dev@sokosumi.com",
-    },
-  });
-
-  if (user) {
-    console.log("User already exists, skipping...");
-    return user.id;
-  }
-
-  user = await prisma.user.create({
-    data: {
-      email: getEnvSecrets().SEED_USER_EMAIL,
-      name: "Sokosumi Developer",
-      emailVerified: true,
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    },
-  });
-  console.log(`User created with email ${user.email}`);
-
-  const password = await hashPassword(getEnvSecrets().SEED_USER_PASSWORD);
-
-  const account = await prisma.account.create({
-    data: {
-      id: uuidv4(),
-      userId: user.id,
-      providerId: "credential",
-      accountId: uuidv4(),
-      password: password,
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    },
-  });
-  console.log(`Account created with id ${account.id}`);
-  return user.id;
-};
-
 const seedUSDMCreditCost = async () => {
-  console.log("Seeding credit cost...");
+  console.log("Seeding USDM credit cost...");
   const unit = usdmUnit(getEnvPublicConfig().NEXT_PUBLIC_NETWORK);
   await prisma.creditCost.upsert({
     where: {
@@ -69,6 +25,7 @@ const seedUSDMCreditCost = async () => {
 };
 
 const seedLoveLaceCreditCost = async () => {
+  console.log("Seeding Lovelace credit cost...");
   await prisma.creditCost.upsert({
     where: {
       unit: "",
@@ -87,7 +44,6 @@ const seedLoveLaceCreditCost = async () => {
 async function main() {
   await seedUSDMCreditCost();
   if (seedDatabase) {
-    await seedUser();
     await seedLoveLaceCreditCost();
   }
 }
