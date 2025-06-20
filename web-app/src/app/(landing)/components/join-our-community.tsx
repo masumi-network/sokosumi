8850c1e5a18cd36543396f8a9e7193342237992f
Golddy Dev <firingdev@gmail.com>
2025-06-20 15:11:07 +0800

    refactor(landing): enhance landing page for improved responsiveness
    - Added a new utility for the landing community background image.
    - Adjusted padding and margin for various sections to enhance mobile responsiveness.
    - Updated text sizes and spacing for better readability across devices.
    - Refactored component structures to utilize flex layouts for improved alignment and spacing.

--- web-app/src/app/(landing)/components/join-our-community.tsx
+++ web-app/src/app/(landing)/components/join-our-community.tsx
@@ -9,14 +9,16 @@ export function JoinOurCommunity() {
 
   return (
     <div className="relative overflow-hidden rounded-lg">
-      <div className="opacity absolute inset-0 bg-[url('/backgrounds/visuals/blurry-ink-wave-1.png')] bg-cover bg-center bg-no-repeat" />
+      <div className="opacity landing-join-our-community-bg absolute inset-0" />
       <div className="bg-background/40 dark:bg-background/40 relative px-6 py-12 sm:px-12 sm:py-16">
         <div className="mx-auto max-w-2xl text-center">
           <p className="mb-2 text-xs tracking-wider uppercase">
             {t("subtitle")}
           </p>
-          <h2 className="mb-8 text-5xl font-light sm:text-4xl">{t("title")}</h2>
-          <div className="flex flex-wrap items-center justify-center gap-4">
+          <h2 className="mb-8 text-2xl font-light sm:text-4xl md:text-5xl">
+            {t("title")}
+          </h2>
+          <div className="flex flex-wrap items-center justify-center gap-1.5 md:gap-4">
             <Button asChild>
               <Link
                 href="https://discord.com/invite/aj4QfnTS92"
