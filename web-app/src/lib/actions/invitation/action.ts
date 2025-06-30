16c9867048c03754e852afb0727321d494211a5f
Golddy Dev <firingdev@gmail.com>
2025-06-30 14:11:18 +0800

    feat: improve error handling in updatePendingInvitations function
    - Refactor to return structured error responses using Result type.
    - Enhance error logging with specific error codes for better debugging.

--- web-app/src/lib/actions/invitation/action.ts
+++ web-app/src/lib/actions/invitation/action.ts
@@ -1,19 +1,24 @@
 "use server";
 
+import { ActionError, CommonErrorCode } from "@/lib/actions/types";
 import { acceptPendingInvitationsByEmailAndOrganizationId } from "@/lib/db/repositories";
+import { Err, Ok, Result } from "@/lib/ts-res";
 
 export async function updatePendingInvitations(
   email: string,
   organizationId: string,
-) {
+): Promise<Result<void, ActionError>> {
   try {
     await acceptPendingInvitationsByEmailAndOrganizationId(
       email,
       organizationId,
     );
-    return { success: true };
+    return Ok();
   } catch (error) {
     console.error("Error updating pending invitations", error);
-    return { success: false };
+    return Err({
+      message: "Error updating pending invitations",
+      code: CommonErrorCode.INTERNAL_SERVER_ERROR,
+    });
   }
 }
