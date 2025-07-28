5aec00a3540b44f3f5797125913a2e9fb603cbee
Andreas Osberghaus <148758+mrgrauel@users.noreply.github.com>
2025-07-28 13:34:21 +0200

    refactor: remove landing page components and related files

--- web-app/src/app/(landing)/components/header/sticky-navigation.tsx
+++
@@ -1,9 +0,0 @@
-import NavigationMenu from "./navigation-menu";
-
-export default function StickyNavigation() {
-  return (
-    <nav className="mx-4 hidden flex-1 md:block">
-      <NavigationMenu className="flex-row items-center justify-center gap-x-6 text-sm font-medium" />
-    </nav>
-  );
-}
--- web-app/src/app/(landing)/terms-of-service/page.tsx
+++
@@ -1,5 +0,0 @@
-import MarkdownFile from "@/components/markdown-file";
-
-export default function TermsOfService() {
-  return <MarkdownFile fileType="terms-of-service" />;
-}
