272403fe37769c65e735592b78389d1ecba26212
Andreas Osberghaus <148758+mrgrauel@users.noreply.github.com>
2025-07-31 07:54:33 +0200

    refactor: update user repository methods to return specific User types

--- web-app/src/lib/db/repositories/user.repository.ts
+++ web-app/src/lib/db/repositories/user.repository.ts
@@ -1,18 +1,18 @@
 import prisma from "@/lib/db/repositories/prisma";
-import { Prisma } from "@/prisma/generated/client";
+import { Prisma, User } from "@/prisma/generated/client";
 
 export const userRepository = {
   getUserById: async (
     id: string,
     client: Prisma.TransactionClient = prisma,
-  ) => {
+  ): Promise<User | null> => {
     return client.user.findUnique({ where: { id } });
   },
 
   getUserByEmail: async (
     email: string,
     client: Prisma.TransactionClient = prisma,
-  ) => {
+  ): Promise<User | null> => {
     return client.user.findUnique({ where: { email } });
   },
 
@@ -20,7 +20,7 @@ export const userRepository = {
     userId: string,
     stripeCustomerId: string | null,
     client: Prisma.TransactionClient = prisma,
-  ) => {
+  ): Promise<User> => {
     return client.user.update({
       where: { id: userId },
       data: { stripeCustomerId },
