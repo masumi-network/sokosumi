923e524c437a76aa239f4211df07fc4fc9d26157
Andreas Osberghaus <148758+mrgrauel@users.noreply.github.com>
2025-08-07 11:07:27 +0200

    fix(jobs): correct refund request condition in RequestRefundButton component

--- web-app/src/app/(app)/agents/[agentId]/jobs/@right/components/job-details/refund-request.tsx
+++ web-app/src/app/(app)/agents/[agentId]/jobs/@right/components/job-details/refund-request.tsx
@@ -142,7 +142,7 @@ export default function RequestRefundButton({
     );
   }
   const isRefundRequested = job.status === JobStatus.REFUND_PENDING;
-  if (!isRefundRequested) {
+  if (isRefundRequested) {
     return (
       <ButtonBase disabled={true} className={className}>
         <LoaderCircle className="h-4 w-4 animate-spin" />
