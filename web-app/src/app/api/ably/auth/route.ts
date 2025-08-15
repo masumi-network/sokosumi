6bb6dd052b5c5653433c36456377903a97d6e78d
Andreas Osberghaus <148758+mrgrauel@users.noreply.github.com>
2025-08-15 13:06:11 +0200

    fix(auth): update session retrieval method to throw error on unauthenticated access

--- web-app/src/app/api/ably/auth/route.ts
+++ web-app/src/app/api/ably/auth/route.ts
@@ -1,11 +1,11 @@
 import { NextResponse } from "next/server";
 
 import createAuthTokenRequest from "@/lib/ably/auth";
-import { getSessionOrRedirect } from "@/lib/auth/utils";
+import { getSessionOrThrow } from "@/lib/auth/utils";
 
 export async function POST() {
   // check user is authenticated
-  const session = await getSessionOrRedirect();
+  const session = await getSessionOrThrow();
 
   const tokenRequest = await createAuthTokenRequest(session.user.id);
   return NextResponse.json(tokenRequest);
