87679ed519e75d53a299484634eb86930fc86a49
Francis Luz <francisluz@gmail.com>
2025-09-29 17:17:50 +0100

    chore(jobs): sync latest job details and config edits

--- web-app/src/config/site.ts
+++ web-app/src/config/site.ts
@@ -7,7 +7,6 @@ export const siteConfig = {
     github: "https://github.com/masumi-network/sokosumi",
     decisionLoggingDocs:
       "https://docs.masumi.network/core-concepts/decision-logging",
-    jobDetails: "https://app.sokosumi.com/jobs/",
     jobTransactionMainnet: "https://cardanoscan.io/transaction/",
     jobTransactionPreprod: "https://preprod.cardanoscan.io/transaction/",
     hydra:
