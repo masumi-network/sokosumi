c625c6d60a2da8cce78b4f43315ec64798935641
Andreas Osberghaus <148758+mrgrauel@users.noreply.github.com>
2025-10-24 13:48:31 +0200

    refactor(api): replace JobShare with JobPublicShare and update related schema and migration files

---
+++ web-app/prisma/migrations/20251024114347_share_refactoring/migration.sql
@@ -0,0 +1,45 @@
+/*
+  Warnings:
+
+  - You are about to drop the `share` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "public"."share" DROP CONSTRAINT "share_creatorId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "public"."share" DROP CONSTRAINT "share_jobId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "public"."share" DROP CONSTRAINT "share_recipientOrganizationId_fkey";
+
+-- AlterTable
+ALTER TABLE "Job" ADD COLUMN     "isOrganizationShared" BOOLEAN NOT NULL DEFAULT false;
+
+-- DropTable
+DROP TABLE "public"."share";
+
+-- CreateTable
+CREATE TABLE "jobPublicShare" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "userId" TEXT NOT NULL,
+    "jobId" TEXT NOT NULL,
+    "token" TEXT NOT NULL,
+    "allowSearchIndexing" BOOLEAN NOT NULL DEFAULT true,
+
+    CONSTRAINT "jobPublicShare_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "jobPublicShare_jobId_key" ON "jobPublicShare"("jobId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "jobPublicShare_token_key" ON "jobPublicShare"("token");
+
+-- AddForeignKey
+ALTER TABLE "jobPublicShare" ADD CONSTRAINT "jobPublicShare_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "jobPublicShare" ADD CONSTRAINT "jobPublicShare_jobId_fkey" FOREIGN KEY ("jobId") REFERENCES "Job"("id") ON DELETE CASCADE ON UPDATE CASCADE;
--- web-app/prisma/migrations/20251024125935_rename_job_share_creator_to_user/migration.sql
+++
@@ -1,6 +0,0 @@
--- AlterTable: Rename creatorId to userId
-ALTER TABLE "share" RENAME COLUMN "creatorId" TO "userId";
-
--- CreateIndex: Add unique constraint on jobId and userId
-CREATE UNIQUE INDEX "share_jobId_userId_key" ON "share"("jobId", "userId");
-
