4c68c7ca17e2e331e6824796629439925b3a0307
Golddy Dev <firingdev@gmail.com>
2025-10-30 23:30:05 +0800

    refactor: add eslint config to root directory

---
+++ eslint.config.mjs
@@ -0,0 +1,6 @@
+import sokosumiConfig from "@sokosumi/eslint-config/base";
+import { defineConfig } from "eslint/config";
+
+const eslintConfig = defineConfig(sokosumiConfig);
+
+export default eslintConfig;
