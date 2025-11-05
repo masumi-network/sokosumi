93f72ac87c988a002bbad531ea21e22dc91d6d53
Francis Luz <francisluz@gmail.com>
2025-11-05 00:50:18 +0000

    fix: database migration

--- packages/database/prisma/migrations/20251104162246_add_priority_to_category/migration.sql
+++ packages/database/prisma/migrations/20251104162246_add_priority_to_category/migration.sql
@@ -1,8 +1,5 @@
 -- AlterTable
-ALTER TABLE "Category" ADD COLUMN "priority" INTEGER NOT NULL DEFAULT 0;
+ALTER TABLE "Category" ADD COLUMN "priority" INTEGER;
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Category_priority_key" ON "Category"("priority");
-
-CREATE INDEX "Category_priority_idx" ON "Category"("priority");
-
