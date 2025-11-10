ffd793cde3c43322ebcdb3fb48b551b149d04d63
Golddy Dev <firingdev@gmail.com>
2025-11-10 08:50:59 +0000

    chore: remove session retry function

--- apps/web/src/lib/auth/auth.client.ts
+++ apps/web/src/lib/auth/auth.client.ts
@@ -5,11 +5,8 @@ import {
   organizationClient,
 } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
-import pTimeout from "p-timeout";
 
-import { sleep } from "@/lib/utils";
-
-import { auth, Session } from "./auth";
+import { auth } from "./auth";
 
 export const authClient = createAuthClient({
   plugins: [
@@ -32,25 +29,3 @@ export const {
   changeEmail,
   changePassword,
 } = authClient;
-
-export const getSessionWithRetry = async (
-  retryCount: number = 5,
-  timeout: number = 5000, // in milliseconds
-  retryDelay: number = 1000, // in milliseconds
-): Promise<Session | null> => {
-  const getSessionWithRetryInner = async (): Promise<Session | null> => {
-    let currentRetryCount = 0;
-    while (currentRetryCount < retryCount) {
-      const session = await authClient.getSession();
-      console.log("Try: ", currentRetryCount, session);
-      if (!session || !session.data) {
-        await sleep(retryDelay);
-        currentRetryCount++;
-      } else {
-        return session.data;
-      }
-    }
-    return null;
-  };
-  return pTimeout(getSessionWithRetryInner(), { milliseconds: timeout });
-};
