8e10e52cfb0a02a03391900943269188c1d9b083
Andreas Osberghaus <148758+mrgrauel@users.noreply.github.com>
2025-11-27 12:14:32 +0100

    feat(index): enhance server initialization for development environment
    - Added conditional server initialization to only run in development mode.
    - Imported 'handle' from 'hono/vercel' for improved server handling.

--- apps/core/src/index.ts
+++ apps/core/src/index.ts
@@ -6,6 +6,7 @@ import { cors } from "hono/cors";
 import { logger } from "hono/logger";
 import type { RequestIdVariables } from "hono/request-id";
 import { requestId } from "hono/request-id";
+import { handle } from "hono/vercel";
 
 import { notFound } from "@/helpers/error";
 import apiV1 from "@/routes/v1/index";
@@ -23,12 +24,16 @@ app.notFound(() => {
 // Mount API v1 routes
 app.route("/v1", apiV1);
 
-serve(
-  {
-    fetch: app.fetch,
-    port: Number(process.env.PORT) || 8787,
-  },
-  (info) => {
-    console.log(`Server is running on http://localhost:${info.port}`);
-  },
-);
+if (process.env.NODE_ENV === "development") {
+  serve(
+    {
+      fetch: app.fetch,
+      port: Number(process.env.PORT) || 8787,
+    },
+    (info) => {
+      console.log(`Server is running on http://localhost:${info.port}`);
+    },
+  );
+}
+
+export default handle(app);
