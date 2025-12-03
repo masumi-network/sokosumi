9393b78451a5f339426b97290459d1f3f918dc77
Andreas Osberghaus <148758+mrgrauel@users.noreply.github.com>
2025-12-03 21:59:11 +0100

    refactor(job-event): change signature type from nullable to non-nullable
    - Updated the signature property in jobEventRepository to be a non-nullable string.
    - This change ensures that the signature is always provided, improving data integrity.

--- packages/database/src/repositories/job-event.repository.ts
+++ packages/database/src/repositories/job-event.repository.ts
@@ -129,7 +129,7 @@ export const jobEventRepository = {
     id: string,
     input: string,
     inputHash: string,
-    signature: string | null,
+    signature: string,
     tx: Prisma.TransactionClient = prisma,
   ): Promise<JobEvent> {
     return await tx.jobEvent.update({
