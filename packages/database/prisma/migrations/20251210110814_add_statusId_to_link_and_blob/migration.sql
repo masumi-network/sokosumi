aea3b1be0d2fe482d085151c4cfade41853ff581
Andreas Osberghaus <148758+mrgrauel@users.noreply.github.com>
2025-12-10 12:44:32 +0100

    fix(database): make jobStatusId and jobInputId nullable in Blob model to allow for more flexible data handling

--- packages/database/prisma/migrations/20251210110814_add_statusId_to_link_and_blob/migration.sql
+++ packages/database/prisma/migrations/20251210110814_add_statusId_to_link_and_blob/migration.sql
@@ -1,12 +1,13 @@
 /*
   Warnings:
 
-  - Made the column `jobStatusId` on table `blob` required. This step will fail if there are existing NULL values in that column.
   - Made the column `jobStatusId` on table `link` required. This step will fail if there are existing NULL values in that column.
 
 */
 -- AlterTable
-ALTER TABLE "blob" ALTER COLUMN "jobStatusId" SET NOT NULL;
+-- Make both jobStatusId and jobInputId nullable (at least one should be set, but both can be null)
+ALTER TABLE "blob" ALTER COLUMN "jobStatusId" DROP NOT NULL;
+ALTER TABLE "blob" ALTER COLUMN "jobInputId" DROP NOT NULL;
 
 -- AlterTable
 ALTER TABLE "link" ALTER COLUMN "jobStatusId" SET NOT NULL;
