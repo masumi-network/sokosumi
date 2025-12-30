5be439c21381f6257b841e3806706ee38248666b
Andreas Osberghaus <148758+mrgrauel@users.noreply.github.com>
2025-12-30 13:57:21 +0100

    Update transformJobInputSchemaValidations to include boolean type in value validation
    - Modified the return type of transformJobInputSchemaValidations to allow boolean values in addition to string and number.
    - Updated the internal validation structure to accommodate the new boolean type for improved flexibility in job input handling.

--- apps/web/src/components/create-job-modal/job-input/util.ts
+++ apps/web/src/components/create-job-modal/job-input/util.ts
@@ -38,11 +38,11 @@ export const transformJobInputSchemaValidations = <
   }
     ? T
     : never,
-): Partial<Record<InputValidation, string | number>> => {
+): Partial<Record<InputValidation, string | number | boolean>> => {
   const { validations } = jobInputSchema as {
     validations?: {
       validation: InputValidation;
-      value: string | number;
+      value: string | number | boolean;
     }[];
   };
   return (validations ?? []).reduce(
@@ -50,6 +50,6 @@ export const transformJobInputSchemaValidations = <
       acc[cur.validation] = cur.value;
       return acc;
     },
-    {} as Partial<Record<InputValidation, string | number>>,
+    {} as Partial<Record<InputValidation, string | number | boolean>>,
   );
 };
