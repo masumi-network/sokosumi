aad25c89da3d16a9a48c5520ee14b7abb05bb222
Andreas Osberghaus <148758+mrgrauel@users.noreply.github.com>
2026-01-09 10:44:49 +0100

    refactor(auth): clear clientId and clientSecret state initialization in OAuthCallbackPage for improved security

--- apps/web/src/app/(auth)/oauth/callback/page.tsx
+++ apps/web/src/app/(auth)/oauth/callback/page.tsx
@@ -32,9 +32,9 @@ export default function OAuthCallbackPage() {
   const t = useTranslations("App.Account.OAuthCallback");
   const searchParams = useSearchParams();
   const [codeVerifier, setCodeVerifier] = useState("");
-  const [clientId, setClientId] = useState("tegKPwxiYvixCtqvpGtwZYzCjYqesbxx");
+  const [clientId, setClientId] = useState("");
   const [clientSecret, setClientSecret] = useState(
-    "soko_client_secret_DHEYETCrpJPazBmRYneRAQqnTDVxjnoL",
+    ""
   );
   const [isExchanging, setIsExchanging] = useState(false);
   const [tokenResponse, setTokenResponse] = useState<TokenResponse | null>(
