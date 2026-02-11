51c197f0a3bef8c2d2b846078f23297e83fe05ee
Francis Luz <francisluz@gmail.com>
2026-02-11 20:19:34 +0000

    style(tasks): format TasksRealtimeListener props for improved readability

--- apps/web/src/app/(app)/tasks/components/tasks-view.tsx
+++ apps/web/src/app/(app)/tasks/components/tasks-view.tsx
@@ -299,7 +299,10 @@ export function TasksView({
       {userId ? (
         <DynamicAblyProvider>
           <ChannelProvider channelName={makeUserTasksChannelName(userId)}>
-            <TasksRealtimeListener userId={userId} onEvent={handleEventUpdate} />
+            <TasksRealtimeListener
+              userId={userId}
+              onEvent={handleEventUpdate}
+            />
           </ChannelProvider>
         </DynamicAblyProvider>
       ) : null}
