fe38f2b0bc3faf4b329d5f84a21e24ad5c0e334c
Andreas Osberghaus <148758+mrgrauel@users.noreply.github.com>
2026-03-03 11:07:45 +0100

    chore(database): drop standalone jobEvent status index

--- packages/database/prisma/migrations/20260302120029_add_job_event_status_hash/migration.sql
+++ packages/database/prisma/migrations/20260302120029_add_job_event_status_hash/migration.sql
@@ -4,6 +4,7 @@ ADD COLUMN "statusHash" TEXT;
 
 -- Remove deprecated status external ID field from job events
 DROP INDEX IF EXISTS "jobEvent_externalId_idx";
+DROP INDEX IF EXISTS "jobEvent_status_idx";
 
 ALTER TABLE "jobEvent"
 DROP COLUMN IF EXISTS "externalId";
