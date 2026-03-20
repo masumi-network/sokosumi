1acef3ebcbb2671e8a9f943ed9438a266042dbcf
Francis Luz <francisluz@gmail.com>
2026-03-20 16:22:18 +0000

    fix(web): improve organization logo resolution in org info view (#2693)

--- apps/core/src/helpers/ipfs.ts
+++
@@ -1,7 +0,0 @@
-export const ipfsUrlResolver = (url: string) => {
-  if (url.startsWith("ipfs://")) {
-    //NMKR IPFS Gateway
-    return url.replace("ipfs://", "https://c-ipfs-gw.nmkr.io/ipfs/");
-  }
-  return url;
-};
